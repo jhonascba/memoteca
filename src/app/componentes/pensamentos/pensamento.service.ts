78d9022758e70a96d384ae92e4a504f074ea996a
jhonascba <jhonascbarbosa@gmail.com>
2023-02-06 20:38:51 -0300

    favoritando um pensamento

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,42 +1,52 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Pensamento } from './pensamento';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Pensamento} from './pensamento';
+import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class PensamentoService {
-  private readonly API = 'http://localhost:3000/pensamentos';
+    private readonly API = 'http://localhost:3000/pensamentos';
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {
+    }
 
-  listar(pagina: number): Observable<Pensamento[]> {
-    const itensPorPagina = 6;
+    listar(pagina: number, filtro: string): Observable<Pensamento[]> {
+        const itensPorPagina = 6;
 
-    let params = new HttpParams()
-      .set('_page', pagina)
-      .set('_limit', itensPorPagina);
+        let params = new HttpParams()
+            .set('_page', pagina)
+            .set('_limit', itensPorPagina);
 
-    return this.http.get<Pensamento[]>(this.API, { params });
-  }
+        if (filtro.trim().length > 2) {
+            params = params.set('q', filtro);
+        }
 
-  criar(pensamento: Pensamento): Observable<Pensamento> {
-    return this.http.post<Pensamento>(this.API, pensamento);
-  }
+        return this.http.get<Pensamento[]>(this.API, {params});
+    }
 
-  editar(pensamento: Pensamento): Observable<Pensamento> {
-    const url = `${this.API}/${pensamento.id}`;
-    return this.http.put<Pensamento>(url, pensamento);
-  }
+    criar(pensamento: Pensamento): Observable<Pensamento> {
+        return this.http.post<Pensamento>(this.API, pensamento);
+    }
 
-  excluir(id: number): Observable<Pensamento> {
-    const url = `${this.API}/${id}`;
-    return this.http.delete<Pensamento>(url);
-  }
+    editar(pensamento: Pensamento): Observable<Pensamento> {
+        const url = `${this.API}/${pensamento.id}`;
+        return this.http.put<Pensamento>(url, pensamento);
+    }
 
-  buscarPorId(id: number): Observable<Pensamento> {
-    const url = `${this.API}/${id}`;
-    return this.http.get<Pensamento>(url);
-  }
+    mudarFavorito(pensamento: Pensamento): Observable<Pensamento> {
+        pensamento.favorito = !pensamento.favorito;
+        return this.editar(pensamento);
+    }
+
+    excluir(id: number): Observable<Pensamento> {
+        const url = `${this.API}/${id}`;
+        return this.http.delete<Pensamento>(url);
+    }
+
+    buscarPorId(id: number): Observable<Pensamento> {
+        const url = `${this.API}/${id}`;
+        return this.http.get<Pensamento>(url);
+    }
 }
